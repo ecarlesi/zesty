b748295b16ec33d3416c678fcf00965ffbab6724
Emiliano Carlesi <emiliano.carlesi@gmail.com>
2020-10-08 00:01:55 +0200

    Update Populate.sql

--- Zesty.Core/Scripts/SqlServer/Populate.sql
+++ Zesty.Core/Scripts/SqlServer/Populate.sql
@@ -32,8 +32,6 @@ INSERT INTO [dbo].[Translation] ([LanguageId],[Original],[Translated]) VALUES ((
 INSERT INTO [dbo].[Translation] ([LanguageId],[Original],[Translated]) VALUES ((select [Id] from [Language] where [Name] = 'it'), 'Reset password', 'Password reset');
 
 INSERT INTO [dbo].[User] ([Id],[Username],[Email],[Firstname],[Lastname],[ResetToken],[Deleted],[Created]) VALUES (newid(),'eca','emiliano.carlesi@gmail.com','Emiliano','Carlesi',null,null,getdate());
-INSERT INTO [dbo].[User] ([Id],[Username],[Email],[Firstname],[Lastname],[ResetToken],[Deleted],[Created]) VALUES (newid(),'leonardo','leotaglio@gmail.com','Leonardo','Taglienti',null,null,getdate());
-INSERT INTO [dbo].[User] ([Id],[Username],[Email],[Firstname],[Lastname],[ResetToken],[Deleted],[Created]) VALUES (newid(),'andrea','andrea.sena.ws@gmail.com','Andrea','Sena',null,null,getdate());
 
 INSERT INTO [dbo].[Role] ([Id],[Name])VALUES (newid(),'Administrators');
 INSERT INTO [dbo].[Role] ([Id],[Name])VALUES (newid(),'Users');
@@ -44,8 +42,6 @@ INSERT INTO [dbo].[Domain] ([Id],[ParentDomainId],[Name])VALUES (newid(),(select
 INSERT INTO [dbo].[Domain] ([Id],[ParentDomainId],[Name])VALUES (newid(),(select [id] from [domain] where [name] = 'Domain X'), 'Domain Y');
 
 INSERT INTO [dbo].[Authorization] ([UserId],[DomainId],[RoleId]) VALUES ((select id from [user] where [username] = 'eca'),(select id from [domain] where [name] = 'Domain A'),(select id from [role] where [name] = 'Administrators'));
-INSERT INTO [dbo].[Authorization] ([UserId],[DomainId],[RoleId]) VALUES ((select id from [user] where [username] = 'leonardo'),(select id from [domain] where [name] = 'Domain A'),(select id from [role] where [name] = 'Administrators'));
-INSERT INTO [dbo].[Authorization] ([UserId],[DomainId],[RoleId]) VALUES ((select id from [user] where [username] = 'andrea'),(select id from [domain] where [name] = 'Domain A'),(select id from [role] where [name] = 'Administrators'));
 
 INSERT INTO [dbo].[Resource] ([Id],[Url],[IsPublic],[RequireToken]) VALUES (newid(),'/Secured/Hello',0,0);
 INSERT INTO [dbo].[Resource] ([Id],[Url],[IsPublic],[RequireToken]) VALUES (newid(),'/sample.private.api',0,0);
@@ -100,8 +96,6 @@ INSERT INTO [dbo].[ResourceType] ([ResourceId],[Type]) VALUES ((select id from [
 INSERT INTO [dbo].[ResourceType] ([ResourceId],[Type]) VALUES ((select id from [Resource] where [Url] = '/system.clientsettings.api'),'Zesty.Core.Api.System.ClientSettings, Zesty.Core');
 
 INSERT INTO [dbo].[UserPassword] ([Id],[UserId],[Password],[Deleted],[Created]) VALUES (newid(),(select id from [user] where [username] = 'eca'),'5E884898DA28047151D0E56F8DC6292773603D0D6AABBDD62A11EF721D1542D8',null,getdate());
-INSERT INTO [dbo].[UserPassword] ([Id],[UserId],[Password],[Deleted],[Created]) VALUES (newid(),(select id from [user] where [username] = 'leonardo'),'5E884898DA28047151D0E56F8DC6292773603D0D6AABBDD62A11EF721D1542D8',null,getdate());
-INSERT INTO [dbo].[UserPassword] ([Id],[UserId],[Password],[Deleted],[Created]) VALUES (newid(),(select id from [user] where [username] = 'andrea'),'5E884898DA28047151D0E56F8DC6292773603D0D6AABBDD62A11EF721D1542D8',null,getdate());
 
 INSERT INTO [dbo].[UserProperties] ([UserId],[Key],[Value]) VALUES ((select id from [user] where [username] = 'eca'),'Prop 1','Value 1');
 INSERT INTO [dbo].[UserProperties] ([UserId],[Key],[Value]) VALUES ((select id from [user] where [username] = 'eca'),'Prop 2','Value 2');

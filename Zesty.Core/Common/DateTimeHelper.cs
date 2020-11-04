28b735ff64ff0a935f04230dccbcc64272924f96
eca <emiliano.carlesi@gmail.com>
2020-11-04 20:48:19 +0100

    datetime helper added

---
+++ Zesty.Core/Common/DateTimeHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Zesty.Core.Common
+{
+    public static class DateTimeHelper
+    {
+        public static double GetUnixTimestamp(DateTime dateTime)
+        {
+            return (TimeZoneInfo.ConvertTimeToUtc(dateTime) - new DateTime(1970, 1, 1, 0, 0, 0, 0, System.DateTimeKind.Utc)).TotalSeconds;
+        }
+
+        public static DateTime GetFromUnixTimestamp(double timestamp)
+        {
+            return TimeZoneInfo.ConvertTimeFromUtc(new DateTime(1970, 1, 1, 0, 0, 0, 0, System.DateTimeKind.Utc).AddSeconds(timestamp), TimeZoneInfo.Local);
+        }
+    }
+}

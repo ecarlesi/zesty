932a22cc30d3dfb3c2b6563c5a3276b362cdbb8c
eca <emiliano.carlesi@gmail.com>
2020-12-04 00:18:23 +0100

    minor fix

--- Zesty.Core/Common/DateTimeHelper.cs
+++ Zesty.Core/Common/DateTimeHelper.cs
@@ -4,12 +4,12 @@ namespace Zesty.Core.Common
 {
     public static class DateTimeHelper
     {
-        public static double GetUnixTimestamp(DateTime dateTime)
+        public static long GetUnixTimestamp(DateTime dateTime)
         {
-            return (TimeZoneInfo.ConvertTimeToUtc(dateTime) - new DateTime(1970, 1, 1, 0, 0, 0, 0, System.DateTimeKind.Utc)).TotalSeconds;
+            return (long)(TimeZoneInfo.ConvertTimeToUtc(dateTime) - new DateTime(1970, 1, 1, 0, 0, 0, 0, System.DateTimeKind.Utc)).TotalSeconds;
         }
 
-        public static DateTime GetFromUnixTimestamp(double timestamp)
+        public static DateTime GetFromUnixTimestamp(long timestamp)
         {
             return TimeZoneInfo.ConvertTimeFromUtc(new DateTime(1970, 1, 1, 0, 0, 0, 0, System.DateTimeKind.Utc).AddSeconds(timestamp), TimeZoneInfo.Local);
         }
